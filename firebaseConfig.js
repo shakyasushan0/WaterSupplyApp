41936deb28ee171409886d6a77b6c66ccca5597f
shakyasushan0 <46255984+shakyasushan0@users.noreply.github.com>
2021-05-31 20:46:44 +0545

    updated firebase config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,1 @@
-import * as firebase from "firebase";
-var firebaseConfig = {
-  apiKey: "AIzaSyAY5byZIJbw74mPf1PRt2fJ5udGaPKpYcE",
-  authDomain: "watersupplyapp-366ed.firebaseapp.com",
-  databaseURL: "https://watersupplyapp-366ed.firebaseio.com",
-  projectId: "watersupplyapp-366ed",
-  storageBucket: "watersupplyapp-366ed.appspot.com",
-  messagingSenderId: "61236132027",
-  appId: "1:61236132027:web:cb9bb7fd863b15e836519e",
-  measurementId: "G-WFC659N754",
-};
-
-export default firebaseConfig;
+Add your firebase config
